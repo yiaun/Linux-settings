74f52a2be10c6cba53893da82b4f7180f6f4e250
yl <36614122+yiaun@users.noreply.github.com>
2023-10-30 20:02:33 +0800

    Update fish_prompt.fish

--- fish/fish_prompt.fish
+++ fish/fish_prompt.fish
@@ -7,6 +7,8 @@ function fish_prompt --description 'Write out the prompt'
     end
     if [ $PWD = $HOME ] ;
         printf "[%s@%s %s%s%s]%s " $USER (hostname -s) (set_color $fish_color_cwd) \~ (set_color normal) $prompt_symbol
+    else if [ $PWD = '/' ];
+        printf "[%s@%s %s%s%s]%s " $USER (hostname -s) (set_color $fish_color_cwd) \/ (set_color normal) $prompt_symbol
     else
         printf "[%s@%s %s%s%s]%s " $USER (hostname -s) (set_color $fish_color_cwd) $pwd (set_color normal) $prompt_symbol
     end
