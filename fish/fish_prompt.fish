835ab5b49387caf9428c15f6c6df030a381643d3
yl <36614122+yiaun@users.noreply.github.com>
2023-11-15 16:40:45 +0800

    Update fish_prompt.fish

--- fish/fish_prompt.fish
+++ fish/fish_prompt.fish
@@ -1,5 +1,5 @@
 function fish_prompt --description 'Write out the prompt'
-    set -l pwd (string replace --regex '^.*/' '' $PWD)
+    set -l pwd (basename $PWD)
     set -l prompt_symbol ''
     switch $USER
         case root toor; set prompt_symbol '#'
@@ -7,8 +7,6 @@ function fish_prompt --description 'Write out the prompt'
     end
     if [ $PWD = $HOME ] ;
         printf "[%s@%s %s%s%s]%s " $USER (hostname -s) (set_color $fish_color_cwd) \~ (set_color normal) $prompt_symbol
-    else if [ $PWD = '/' ];
-        printf "[%s@%s %s%s%s]%s " $USER (hostname -s) (set_color $fish_color_cwd) \/ (set_color normal) $prompt_symbol
     else
         printf "[%s@%s %s%s%s]%s " $USER (hostname -s) (set_color $fish_color_cwd) $pwd (set_color normal) $prompt_symbol
     end
