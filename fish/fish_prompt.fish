e102e8298c36748aabb5d061ba03781bee01501a
yl <36614122+yiaun@users.noreply.github.com>
2023-11-15 17:26:29 +0800

    Update fish_prompt.fish

--- fish/fish_prompt.fish
+++ fish/fish_prompt.fish
@@ -1,13 +1,9 @@
 function fish_prompt --description 'Write out the prompt'
-    set -l pwd (basename $PWD)
+    set -l pwd (basename (string replace --regex '/root' '~' $PWD))
     set -l prompt_symbol ''
     switch $USER
         case root toor; set prompt_symbol '#'
         case '*'; set prompt_symbol '$'
     end
-    if [ $PWD = $HOME ] ;
-        printf "[%s@%s %s%s%s]%s " $USER (hostname -s) (set_color $fish_color_cwd) \~ (set_color normal) $prompt_symbol
-    else
-        printf "[%s@%s %s%s%s]%s " $USER (hostname -s) (set_color $fish_color_cwd) $pwd (set_color normal) $prompt_symbol
-    end
+    printf "[%s@%s %s%s%s]%s " $USER (hostname -s) (set_color $fish_color_cwd) $pwd (set_color normal) $prompt_symbol
 end
