8eab113e7a2d18d31b35c610f7d4acb95db809ca
yl <36614122+yiaun@users.noreply.github.com>
2025-06-09 09:41:26 +0800

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,8 @@
-
 if status is-interactive
+    abbr less less -MXF
+    abbr el exa -gl
+    abbr gg getent group
+    set -x TIME_STYLE long-iso
+    source /usr/share/autojump/autojump.fish
     # Commands to run in interactive sessions can go here
-    set -x TIME_STYLE +%F %H:%M
-    abbr l ls -clhF
 end
